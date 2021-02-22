767b5d8bfa9653cb2473f24e5e852d3c8abb5f1f
Christina <patriciasteffen@gmx.ch>
2021-02-22 13:03:57 +0100

    gibts conflict?

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-function duplicateCount(text){
-  //...
+function digPow(n, p){
+  // ...
+  let arr = n.toString().split('').map(Number);
+  let result = function factorial
+  console.log(arr[2]);
+}
+
+function pow(x) {
+  
+}
+
+
+
+function duplicateCount(text) {
+    // this function will count duplicates in a string (i.e. aaeeiar will return 2)
   let words = text.toLowerCase().split('');
-  let count = 0;
-  let x = 0;
-  for (let i = 0; i < text.length; i++) {
-    for (let j = 0; j < text.length; j++) {
-        if (words[i] === words[j]) {
-          x++;
-          }
-      }
-    if (x>1) {
-      count++;
-    }
-    x=0;
-    }
-    return count/2;
+  let counts = {},
+  duplicate = 0;
+  words.forEach(function(x) {
+  counts[x] = (counts[x] || 0) + 1;
+});
+
+for (var key in counts) {
+  if (counts.hasOwnProperty(key)) {
+    counts[key] > 1 ? duplicate++ : duplicate;
   }
+}
+
+return duplicate;
+}
 
 function tryAgain() {
   alert("Hello! I am an alert box!!");
@@ -25,11 +37,9 @@ function myFunction() {
   document.getElementById("demo").innerHTML = "Hello World";
 }
 
+let testDuplicate = "aaBBbcde";
+let testPow = [12433, 2];
 
-
-let test = "aaBBbcde";
-
-
-console.log(duplicateCount("indivisibility"));
+console.log(duplicateCount("indivisibbbillity"));
 console.log(duplicateCount("abBcde"));
 console.log(duplicateCount("aabbcde"));
